78cab62d8821f1414cb8852b3cb57547d466cada
Eugene <eako0508@gmail.com>
2018-02-07 17:34:36 -0500

    change app to {app} from test.js

--- test/test.js
+++ test/test.js
@@ -3,11 +3,12 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHttp);
+const {app} = require('../server.js');
 
 describe('Server running test', function(){
 	describe('Is the server running', function(){
 		it('should display index.html with status 200', function(){
-			return chai.request('http://localhost:8080')
+			return chai.request(app)
 			  .get('/')
 			  .then(res=>{
 		  		expect(res).to.have.status(200);
