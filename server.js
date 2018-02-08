6bf3c8c37c2eaa18876acae11569386b17e53ae8
eako0508 <eako0508@gmail.com>
2018-02-07 22:52:07 -0500

    added router for /users endpoint and tested

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+const {router: userRouter} = require('./users');
+//app.use(express.static('public'));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+app.use('/', express.static('public'));
+app.use('/users', userRouter);
 app.listen(process.env.PORT || 8080);
 
 module.exports = {app};
--- test/test.js
+++ test/test.js
@@ -6,10 +6,19 @@ chai.use(chaiHttp);
 const {app} = require('../server.js');
 
 describe('Server running test', function(){
-	describe('Is the server running', function(){
+	describe('localhost:8080', function(){
 		it('should display index.html with status 200', function(){
 			return chai.request(app)
 			  .get('/')
+			  .then(res=>{
+		  		expect(res).to.have.status(200);
+			  });
+		});
+	});
+	describe('localhost:8080/users', function(){
+		it('should return status 200', function(){
+			return chai.request(app)
+			  .get('/users')
 			  .then(res=>{
 		  		expect(res).to.have.status(200);
 			  });
