c323cb912a0508ea2b283ff79a08c2aade233e86
eako0508 <eako0508@gmail.com>
2018-02-07 22:02:16 -0500

    loaded both map and places api and make quick api request

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,9 +1,33 @@
-
+/**
+	GOOGLE MAP
+**/
 var map;
-
+let service;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat:40.727141, lng: -73.907959},
 		zoom: 12
 	});
+	service = new google.maps.places.PlacesService(map);
 }
+/**
+	GOOGLE PLACE
+**/
+
+//let service = new google.maps.places.PlacesService(map);
+//service.nearbySearch(request, callback);
+function callback(results, status){
+	if(status === google.maps.places.PlacesServiceStatus.OK){
+		console.log(results);
+	}
+}
+
+//event handler
+$('#custom_query_submit').on('click', function(event){
+	event.preventDefault();
+	service.nearbySearch({
+		location: {lat:40.727141, lng: -73.907959},
+		radius: 500,
+		type: ['store']
+	}, callback);
+});
