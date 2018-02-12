f7e2b4dbafd91acc76497fef538cd180a7ca4ef5
eako0508 <eako0508@gmail.com>
2018-02-11 19:26:36 -0500

    show saved lists with mock data

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,58 +1,5 @@
 //user.js
-
-
-function renderData(data){
-	const data_list = data.saved_list.map((item, index) => {
-		renderList(item,index);
-		renderList_group(item, index);
-		renderList_content(item, index);
-	});
-	$('#user-info').html(data_list);
-}
-
-function renderList_group(item, index){
-	//
-}
-
-function renderList(item, index){
-	return `
-		<div id='user-saved-lists-${index}' class='card-header'>
-	        <button class="btn btn-primary" data-toggle="collapse" 
-	        data-target="#saved-list-${index}">${item.title}</button>
-		    <div id="saved-list-${index}" class="collapse" data-parent="#user-info">
-			    <div class="card-body">
-			    	<div class='row'>
-			    		<div class='col-4'>
-			    			<div class='list-group list-group-${index}'>
-//needs looping
-			    				<a class="list-group-item list-group-item-action" data-toggle="list" href="#list-1">Ice Cream</a>
-			    				<a class="list-group-item list-group-item-action" data-toggle="list" href="#list-2">Movie Theater</a>
-			    				<a class="list-group-item list-group-item-action" data-toggle="list" href="#list-3">Restaurant</a>
-
-			    			</div>
-			    		</div>
-			    		<div class='col-8'>
-			    			<div class='tab-content tab-content-${index}'>
-//needs looping
-			    				<div class="tab-pane fade" id="list-1" >
-			    					<div class='position'>alt: 1, lnt: 1</div>
-			    				</div>
-			    				<div class="tab-pane fade" id="list-2" >
-			    					<div class='position'>alt: 1, lnt: 2</div>
-			    				</div>
-			    				<div class="tab-pane fade" id="list-3" >
-			    					<div class='position'>alt: 1, lnt: 3</div>
-			    				</div>
-
-			    			</div>
-			    		</div>
-			    	</div>
-			    </div>
-		    </div>
-		</div>
-	`;
-}
-
+'use strict'
 
 let userData = {
 	"saved_list": [
@@ -100,4 +47,92 @@ let userData = {
 		}
 	]
 };
+
+function renderData(data){
+	
+	const data_list = data.saved_list.map((item, index) => {
+		//return onemorefunction(item, index);
+		return renderList(item,index);
+		//renderList_group(item, index);
+		//renderList_content(item, index);
+	});
+	
+	$('#user-info').html(data_list);
+	//console.log(data_list);
+}
+/*
+function renderList_group(item, index){
+	return `
+		<a class="list-group-item list-group-item-action" 
+		data-toggle="list" href="#list-${index}">Ice Cream</a>
+	`;
+}
+
+function render_content(item, index){
+	return `
+		<div class="tab-pane fade" id="list-1" >
+			<div class='position'>alt: 1, lnt: 1</div>
+		</div>
+		<div class="tab-pane fade" id="list-2" >
+			<div class='position'>alt: 1, lnt: 2</div>
+		</div>
+		<div class="tab-pane fade" id="list-3" >
+			<div class='position'>alt: 1, lnt: 3</div>
+		</div>
+	`;
+}
+*/
+function renderList(item, index){
+		
+
+
+	let result = `
+		<div id='user-saved-lists-${index}' class='card-header'>
+	        <button class="btn btn-primary" data-toggle="collapse" 
+	        data-target="#saved-list-${index}">${item.title}</button>`;
+
+   	result += `
+	    <div id="saved-list-${index}" class="collapse" data-parent="#user-info">
+		    <div class="card-body">
+		    	<div class='row'>
+		    		<div class='col-4'>
+		    			<div class='list-group' id='list-group-${index}'>`;
+		    			
+	let contents = '';
+
+	for(let i=0;i<item.entries.length;i++){
+		result += `<a class="list-group-item list-group-item-action" 
+		data-toggle="list" href="#list-${index}-${i}">${item.entries[i].name}</a>`;
+		
+		contents += `
+			<div class="tab-pane fade" id="list-${index}-${i}" >
+				<div class='position'>
+					alt: ${item.entries[i].coord.alt}, 
+					lnt: ${item.entries[i].coord.lnt} 
+				</div>
+			</div>
+		`;
+		
+	}
+
+	result += `
+			    			</div>
+			    		</div>
+			    		<div class='col-8'>
+			    			<div class='tab-content tab-content-${index}'>`;
+	result += contents;
+	result += `
+			    			</div>
+			    		</div>
+			    	</div>
+			    </div>
+		    </div>
+		</div>
+	`;
+	return result;
+	
+}
+
+$(renderData(userData));
+
 
