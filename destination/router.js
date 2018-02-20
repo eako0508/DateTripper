e503a2a6f3b8a61112c027e655d9560e3bec2077
eako0508 <eako0508@gmail.com>
2018-02-19 23:35:27 -0500

    2.19.18

--- destination/router.js
+++ destination/router.js
@@ -34,35 +34,19 @@ router.route('/find/:username')
 
 //check if the same title exists
 //if not, create an entry
-/*
-router.route('/addDate/:username', (req,res)=>{
-  return Destination
-    .find({title:req.body.title})
-    .count()
-    .then(count=>{
-      if(count>0){
-        return Promise.reject({
-          code: 422,
-          message: 'title conflict'
-        });
-      }
-      next();
-    })
-    .then(
-      Destination.create({
-        username: req.params.username,
-        title: req.body.title,
-        destinations: req.body.destinations
+
+router.route('/addDate/:username')
+  .post((req,res)=>{
+  	Destination
+      .find({
+        title: req.body.title
       })
-      .then(destination => res.status(201).json(destination.serialize()))
-      .catch(err=>{
-        console.error(err);
-        res.status(500).send('Server error');
-      })
-      );
-});
-*/
-/*
+      .count()
+      .then(result=>{
+        if(result>0){
+          //Q: how to display the error message??
+          res.status(500).send('so sorry');
+        }
         Destination.create({
           username: req.params.username,
           title: req.body.title,
@@ -73,45 +57,9 @@ router.route('/addDate/:username', (req,res)=>{
           console.error(err);
           res.status(500).send('Server error');
         });
-        */
-
-router.route('/addDate/:username')
-  .post((req,res)=>{
-  	Destination
-      .find({
-        title: req.body.title
-      })
-      .count()
-      .then(result=>{
-        
-        
-        if(result>0){
-          console.log(result);
-          /*
-          return Promise.reject({
-            code: 422,
-            message: 'Same title exists',
-            location: 'title'
-          });
-          
-          return res.status(500).json(JSON.stringify({
-            "message": 'Same title exists',
-            "location": 'title'
-          }));
-          */
-          res.status(500).send('so sorry');
-        }
-        
-        res.status(201).send('a okay');
       });  	
   });
 
-/*
-router.route('/:username').post(function(req,res){
-	console.log(req.body.title);
-	//console.log('hello');
-});
-*/
 
 
 module.exports = {router};
