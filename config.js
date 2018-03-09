c08edc498553c1dd1cc85aa077fd08af9bdffafa
eako0508 <eako0508@gmail.com>
2018-03-08 20:58:32 -0500

    chagned db url

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 'use strict';
-//exports.DATABASE_URL = 'mongodb://admin:admin@ds155288.mlab.com:55288/datetripper';
-exports.DATABASE_URL = 'mongodb://localhost/datetripper';
+exports.DATABASE_URL = 'mongodb://admin:admin@ds155288.mlab.com:55288/datetripper';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
