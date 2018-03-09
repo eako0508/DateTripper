7db9b85be2cf796fbc8bda07cff882346f62d58e
eako0508 <eako0508@gmail.com>
2018-03-08 23:06:47 -0500

    template for contact since no internet...

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = 'mongodb://admin:admin@ds155288.mlab.com:55288/datetripper';
+exports.DATABASE_URL = 'mongodb://localhost/datetripper' || process.env.DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
